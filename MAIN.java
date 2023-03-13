1f75d314a219817884a5d4676420be41d0aa624a
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-13 16:29:42 -0500

    update

--- MAIN.java
+++ MAIN.java
@@ -1,10 +1,13 @@
 public class MAIN {
     public static void main(String[] args) {
-        for (int i = 5; i > 3 ; --i) {
-            for (int j = 1; j <=2 ; j++) {
-                System.out.println(j);
+        for(int i=1;i<=3;i++){
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
             }
-            System.out.println(i);
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
 }
