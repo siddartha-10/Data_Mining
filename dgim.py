471fc183c815c7f26ba634ff61642ad825bcabe4
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-29 17:43:27 -0500

    Create dgim.py

---
+++ dgim.py
@@ -0,0 +1,24 @@
+def input_bin_stream(Input):
+    binary_file = open("Binary_Input.txt", "w") #this file stores the 0/1 stream
+    bin_stream = []
+
+    # converting the letters to 0/1 stream
+    for i in Input:
+        if (ord(i) in range(65, 91) or ord(i) in range(97, 123)):
+            if (ord(i) % 2 == 0):
+                bin_stream.append(0)
+            else:
+                bin_stream.append(1)
+
+    # storing the binary stream into the Binary_Input file
+    count = 0
+    for x in bin_stream:
+        binary_file.write("%i" % x)
+        count = count + 1
+        if (count == 32):
+            binary_file.write("\n")
+            count = 0
+
+    binary_file.close()
+    return bin_stream
+string_input = input("Hey please enter the string")
