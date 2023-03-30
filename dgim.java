38fa3d4373c7fd401556af49123a33d45936ad92
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-29 21:36:33 -0500

    commit

--- dgim.java
+++ dgim.java
@@ -1,96 +1,113 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * Created by michiline on 17/05/2017.
+ */
 public class dgim {
 
-    public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bwr = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            List<Integer> results = new ArrayList<>();
-            int currentTimestamp = -1;
-            List<Bucket> buckets = new LinkedList<>();
-            int N = Integer.parseInt(br.readLine().strip());
-            String line;
-            while ((line = br.readLine()) != null) {
-                line = line.strip();
-                if (line.startsWith("q")) {
-                    int k = Integer.parseInt(line.split("\\s+")[1]);
-                    int sum = 0;
-                    int timestamp = currentTimestamp - k;
-                    Bucket lastBucket = null;
-                    for (int i = buckets.size() - 1; i >= 0; i--) {
-                        Bucket bucket = buckets.get(i);
-                        if (bucket.timestamp > timestamp) {
-                            sum += bucket.cnt;
-                            lastBucket = bucket;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (lastBucket != null) {
-                        sum -= lastBucket.cnt;
-                        sum += lastBucket.cnt / 2;
-                    }
-                    results.add(sum);
-                } else {
-                    for (char c : line.toCharArray()) {
-                        currentTimestamp++;
-                        if (!buckets.isEmpty() && currentTimestamp - buckets.get(0).timestamp >= N) {
-                            buckets.remove(0);
-                        }
-                        if (c == '0') continue;
-                        Bucket bucket = new Bucket(currentTimestamp);
-                        buckets.add(bucket);
-                        if (buckets.size() > 2) {
-                            int i = buckets.size() - 2;
-                            while (true) {
-                                Bucket ithBucket = buckets.get(i);
-                                Bucket ithPreviousBucket = buckets.get(i - 1);
-                                if (ithPreviousBucket.cnt == ithBucket.cnt) {
-                                    ithBucket.cnt *= 2;
-                                    buckets.remove(i - 1);
-                                }
-                                i--;
-                                if (i <= 1) break;
-                                i--;
-                                Bucket ithNextBucket = buckets.get(i + 1);
-                                ithBucket = buckets.get(i);
-                                ithPreviousBucket = buckets.get(i - 1);
-                                if (!(ithPreviousBucket.cnt == ithNextBucket.cnt
-                                        && ithBucket.cnt == ithNextBucket.cnt)) break;
-                            }
-                        }
+    private static class Bucket {
+        public int timestamp;
+        public int size;
 
+        public Bucket(int timestamp, int size) {
+            this.timestamp = timestamp;
+            this.size = size;
+        }
+    }
+
+    private static class Buckets {
+        // window length
+        private int N;
+        private ArrayList<Bucket> items;
+
+        public Buckets(int N) {
+            this.N = N;
+            this.items = new ArrayList<>();
+        }
+
+        public void checkWindow(int currentTime) {
+            int difference = currentTime - N;
+            items = items.stream().filter(i -> i.timestamp > difference).collect(Collectors.toCollection(ArrayList::new));
+        }
+
+        public void addNewBucket(Bucket b, int currentTime) {
+            ArrayList<Bucket> temp = new ArrayList<>();
+            int length = items.size();
+            for (int i = 0; i < length; i++) {
+                if (items.get(i).size == b.size) {
+                    temp.add(items.get(i));
+                    if (temp.size() == 2) {
+                        break;
                     }
                 }
             }
-            for (Integer result : results) {
-                bwr.write(result + System.lineSeparator());
+            items.add(b);
+            if (temp.size() != 0 && temp.size() != 1) {
+                items.remove(temp.get(0));
+                items.remove(temp.get(1));
+                int timestamp = Math.max(temp.get(0).timestamp, temp.get(1).timestamp);
+                int size = temp.get(0).size * 2;
+                Bucket newBucket = new Bucket(timestamp, size);
+                addNewBucket(newBucket, currentTime);
             }
         }
-    }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            final long start = System.nanoTime();
 
-            @Override
-            public void run() {
-                System.err.printf(Locale.US, "Runtime: %.6fs%n", (System.nanoTime() - start) * 1e-9);
+        public int estimateOnes(int k, int currentTime) {
+            int difference = currentTime - k;
+            int length = items.size();
+            int sum = 0;
+            int max = 0;
+            for (int i = 0; i < length; i++) {
+                Bucket b = items.get(i);
+                if (b.timestamp >= difference) {
+                    sum += b.size;
+                    if (b.size > max) {
+                        max = b.size;
+                    }
+                }
             }
-        }));
-    }
-
-    private static class Bucket {
-        private int timestamp;
-        private int cnt;
-
-        private Bucket(int timestamp) {
-            this.timestamp = timestamp;
-            this.cnt = 1;
+            if (max == 1) {
+                max++;
+            }
+            return sum - max / 2;
         }
     }
 
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new FileReader("examples/1.in"));
+//        BufferedWriter bw = new BufferedWriter(new FileWriter("examples/my.out"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String line;
+        Buckets buckets = new Buckets(N);
+        int time = 0;
+
+
+        while ((line = br.readLine()) != null) {
+            if (line.startsWith("q")) {
+                int k = Integer.parseInt(line.split(" ")[1]);
+                int est = buckets.estimateOnes(k, time);
+                System.out.println(est);
+//                bw.write(est + "\n");
+//                bw.flush();
+            } else {
+                int length = line.length();
+                for (int i = 0; i < length; i++) {
+                    char c = line.charAt(i);
+                    buckets.checkWindow(time);
+                    if (c == 49) {
+                        buckets.addNewBucket(new Bucket(time, 1), time);
+                    }
+                    time++;
+                }
+            }
+        }
+        br.close();
+//        bw.close();
+    }
 }
--- dgim.py
+++ dgim.py
@@ -1,26 +1,147 @@
-def input_bin_stream(Input):
-    binary_file = open("Binary_Input.txt", "w") #this file stores the 0/1 stream
-    bin_stream = []
+# input1 = list(map(int,input("Please input the data stream separated by spaces ").split()))
+# print(len(input1))
+# print("Input is ", input1)
+# k = int(input("Enter the value of k : "))
+# bucket_list = []
+# count_bucketsize = {}
 
-    # converting the letters to 0/1 stream
-    for i in Input:
-        if (ord(i) in range(65, 91) or ord(i) in range(97, 123)):
-            if (ord(i) % 2 == 0):
-                bin_stream.append(0)
+# start_ind =0
+# end_ind = 0
+# pair = 0
+
+# for i in range(len(input1)):
+#     binary_bit = input1[i]
+#     if(binary_bit == 1):
+#         if(pair == 1):
+#             end_ind = i
+#             pair = 0
+#             bucket_list.append((start_ind,end_ind,2))
+#             if 2 in count_bucketsize:
+#                 count_bucketsize[2]+=1
+#             else:
+#                 count_bucketsize[2] = 1
+#             for ct in count_bucketsize.keys():
+#                 if(count_bucketsize[ct]>2):
+#                     s2,e2,size2 = bucket_list.pop(-2)
+#                     s1,e1,size1 = bucket_list.pop(-2)
+#                     bucket_list.insert(-1,(s1,e2,size1*2))
+#                     count_bucketsize[ct]-=2
+#         else:
+#             start_ind = i
+#             pair = 1
+
+
+# print ("List of Bucket Indexes Are : ",bucket_list)
+# starts = []
+# ends = []
+# for s,e,size in bucket_list:
+#     starts.append(s)
+#     ends.append(e)
+
+# print ("Buckets are ",end="")
+
+# for i in range(len(input1)):
+#     binary_bit = input1[i]
+#     if(i in starts):
+#         print (" " ,binary_bit,end="")
+#     elif(i in ends):
+#         print (binary_bit,end= " ")
+#     else:
+#         print (binary_bit,end = " ")
+
+# print("")
+
+
+# length = len(input1)
+
+# bound1 = length-1-k
+# bound2 = length-1
+
+# count_1 = 0
+
+# for s,e,size in bucket_list[::-1]:
+#     if(s<bound1 and e < bound1):
+#         break
+#     elif(s<=bound1 and e >= bound1):
+#         count_1 +=int(size/2)
+#     elif(s>=bound1 and e >= bound1):
+#         count_1 += size
+#     else:
+#         continue
+
+
+
+# print("Last",k,"binary bits have",count_1,"Ones")
+
+
+
+from collections import defaultdict
+
+def add_timestamp(bucket_timestamps, bucket_counts, timestamp, bucket):
+    timestamp_next_bucket = bucket_timestamps[bucket][1]
+    if (bucket_timestamps[bucket][0] == 0 and bucket_timestamps[bucket][1] == 0) or \
+            (bucket_timestamps[bucket][0] != 0 and bucket_timestamps[bucket][1] != 0):
+        bucket_timestamps[bucket][0] = timestamp
+        bucket_timestamps[bucket][1] = 0
+    elif bucket_timestamps[bucket][0] != 0 and bucket_timestamps[bucket][1] == 0:
+        bucket_timestamps[bucket][1] = timestamp
+    if timestamp_next_bucket != 0:
+        bucket_counts[bucket*2] += 1
+        bucket_counts[bucket] = 1
+    return timestamp_next_bucket
+
+def remove_bucket_timestamp(bucket_timestamps, bucket):
+    if bucket_timestamps[bucket][0] != 0 and bucket_timestamps[bucket][1] != 0:
+        bucket_timestamps[bucket][0] = bucket_timestamps[bucket][1]
+        bucket_timestamps[bucket][1] = 0
+    elif bucket_timestamps[bucket][0] != 0 and bucket_timestamps[bucket][1] == 0:
+        del bucket_timestamps[bucket]
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    s = input().strip()
+    while s == "":
+        s = input().strip()
+    a = []
+    n = len(s)
+    for i in range(n):
+        if s[i].isalpha():
+            if (ord(s[i]) - ord('0')) & 1:
+                a.append(1)
             else:
-                bin_stream.append(1)
-
-    # storing the binary stream into the Binary_Input file
-    count = 0
-    for x in bin_stream:
-        binary_file.write("%i" % x)
-        count = count + 1
-        if (count == 32):
-            binary_file.write("\n")
+                a.append(0)
+    timestamp = 1
+    bucket_counts = defaultdict(int)
+    bucket_timestamps = defaultdict(lambda: [0, 0])
+    bucket_index_merge = 0
+    siz = len(a)
+    for i in range(siz):
+        if bucket_timestamps:
+            end_bucket = max(bucket_timestamps.items())
+            end_timestamp = end_bucket[1][0]
+            if timestamp - end_timestamp >= N:
+                bucket_counts[end_bucket[0]] -= 1
+                if bucket_counts[end_bucket[0]] == 0:
+                    del bucket_counts[end_bucket[0]]
+                remove_bucket_timestamp(bucket_timestamps, end_bucket[0])
+        if a[i] == 1:
+            bucket_counts[1] += 1
+            for bucket, count in bucket_counts.copy().items():
+                if bucket == 1:
+                    bucket_index_merge = add_timestamp(bucket_timestamps, bucket_counts, timestamp, bucket)
+                elif bucket != 1 and bucket_index_merge != 0:
+                    bucket_index_merge = add_timestamp(bucket_timestamps, bucket_counts, bucket_index_merge, bucket)
+                else:
+                    break
+        count_print_check = 0
+        if i > siz - len("straightforward") - 1:
             count = 0
-
-    binary_file.close()
-    print(len(bin_stream))
-    return bin_stream
-string_input = input("Hey please enter the string \n")
-print(string_input)
+            for bucket, bucket_count in bucket_counts.items():
+                if bucket == max(bucket_counts):
+                    count += bucket // 2
+                    count_print_check += 1
+                    break
+                count += bucket * bucket_count
+            if count_print_check == 1:
+                print(count)
+        timestamp += 1
